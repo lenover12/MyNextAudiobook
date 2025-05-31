9c2bce8d06a653cc36119cf9f56c3029bef7b9ed
lenover12 <lennymcdonald247@hotmail.com>
2025-05-31 18:28:36 +1000

    Filter explicit in search

--- src/utils/itunesAPI.ts
+++ src/utils/itunesAPI.ts
@@ -12,14 +12,22 @@ interface FetchOptions {
 
 export async function fetchRandom(options?: FetchOptions): Promise<any | null> {
   const term = await getSearchTerm(options);
-  console.log(term);
-
   const offset = Math.floor(Math.random() * 200);
   const limit = Math.min(25, 200 - offset);
-  const url = `https://itunes.apple.com/search?term=${term}&media=audiobook&limit=${limit}`
+  const explicitParam = options?.allowExplicit ? 'yes' : 'no';
+
+  console.log(term);
+  
+  const url = `https://itunes.apple.com/search?term=${term}&media=audiobook&limit=${limit}&explicit=${explicitParam}`;
+
   const response = await fetch(url);
   const data = await response.json();
-  const results = data.results.filter((item: any) => item.previewUrl);
+
+  const results = data.results.filter((item: any) =>
+    item.previewUrl &&
+    (options?.allowExplicit || item.collectionExplicitness === 'notExplicit')
+  );
+
   if (results.length === 0) return null;
   return results[Math.floor(Math.random() * results.length)];
 }
