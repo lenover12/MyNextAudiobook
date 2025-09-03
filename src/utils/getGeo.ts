cc242d2a2fb34c5ccd7a76c8b20512ae4da9e2bf
lenover12 <lennymcdonald247@hotmail.com>
2025-09-04 02:01:57 +1000

    Update getGeo to use or set options

--- src/utils/getGeo.ts
+++ src/utils/getGeo.ts
@@ -1,12 +1,23 @@
+import { loadOptions, saveOptions } from "./optionsStorage";
+
 let cachedCountry: string | null = null;
 
 export async function getCountryCode(): Promise<string> {
+  const options = loadOptions();
+  if (options.countryCode) return options.countryCode.toLowerCase();
   if (cachedCountry) return cachedCountry;
   try {
     const res = await fetch('https://ipapi.co/json');
     const data = await res.json();
-    //fallback is US
-    return data.country || 'us';
+    const country = data.country?.toLowerCase();
+
+    if (country) {
+      cachedCountry = country;
+      saveOptions({ ...options, countryCode: country });
+      return country;
+    } else {
+      return 'us'
+    }
   } catch (e) {
     console.warn('Failed to fetch country code:', e);
     return 'us';
